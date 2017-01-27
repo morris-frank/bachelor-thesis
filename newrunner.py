62f68757926f6d19824ed5be973eacadc8581e79
Tukkin Moriarty <mrtukkin@gmail.com>
2017-01-27 01:46:29 +0100

    Finished PascalPartSet..)

--- newrunner.py
+++ newrunner.py
@@ -1,7 +1,12 @@
 import ba.FCNPartRunner
+import ba.PascalPartSet
 
 traintxt = 'data/datasets/pascalparts/train.txt'
 valtxt = 'data/datasets/pascalparts/val.txt'
+sourcedir = 'data/datasets/pascalparts/...'
+
+ppset = PascalPartSet('pascpart', sourcedir, 'tail', 'aeroplane')
+ppset.saveSegmentations()
 
 fcn = ba.FCNPartRunner('v1', traintxt, valtxt, samples=100)
 fcn.baselr = 10e-4
