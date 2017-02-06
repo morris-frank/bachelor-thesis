097cc23045ef77febbbe4fac682699779ecb516b
Tukkin Moriarty <mrtukkin@gmail.com>
2017-02-07 00:40:29 +0100

    Added documentation to set

--- ba/set.py
+++ ba/set.py
@@ -8,8 +8,14 @@ from ba import utils
 
 
 class SetList(object):
-    # TODO(doc): Add docstring
+    '''A class to hold lists of inputs for a network'''
+
     def __init__(self, source=''):
+        '''Constructs a new SetList.
+
+        Args:
+            source (str): The path to the list file
+        '''
         self.source = source
         self.target = source
         self.list = []
@@ -18,33 +24,30 @@ class SetList(object):
             self.load()
 
     def __len__(self):
-        # TODO(doc): Add docstring
+        '''Returns the length of this Set'''
         return len(self.list)
 
     def __str__(self):
-        # TODO(doc): Add docstring
+        '''Returns a str-description of this Set'''
         return '{}[{}] → {}'.format(self.source, len(self.list), self.target)
 
     def load(self):
-        '''Loads the contents of self.source into the list.
-        It does replace the whole content and does not append to it.
-        '''
+        '''Loads the contents of self.source into the list. It does replace the
+        whole content and does not append to it.'''
         utils.touch(self.source)
         with open(self.source) as f:
             self.list = [l[:-1] for l in f.readlines() if l.strip()]
 
     def save(self):
-        '''Saves the list to the path set in self.target.
-        This is normally set to self.source
-        '''
+        '''Saves the list to the path set in self.target. This is normally set
+        to self.source'''
         with open(self.target, 'w') as f:
             for row in self.list:
                 f.write("{}\n".format(row))
         print('List {} written...'.format(self.target))
 
     def shuffle(self):
-        '''Shuffles the list
-        '''
+        '''Shuffles the list'''
         random.shuffle(self.list)
 
     def addPreSuffix(self, prefix='', suffix=''):
@@ -60,13 +63,19 @@ class SetList(object):
         '''Removes a prefix and a suffix from every element of the list.
 
         Args:
-        prefix (str,optional): The prefix to remove
-        suffix (str,optional): The prefix to remove
+            prefix (str,optional): The prefix to remove
+            suffix (str,optional): The prefix to remove
         '''
         self.list = [x[len(prefix):-len(suffix)] for x in self.list]
 
     def calculate_mean(self):
-        # TODO(doc): Add docstring
+        '''Calculates the mean pixel for this set. The list has to contain full
+        paths obviously so you probably have to append Prefixes and suffixes
+        before running this.
+
+        Returns:
+            The mean pixel.
+        '''
         self.mean = [[],[],[]]
         print('Calculating mean pixel...')
         for row in tqdm(self.list):
