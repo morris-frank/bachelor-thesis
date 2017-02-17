26705a4e7731956c362ff20731aa8dd4a9b7dd5c
Maurice Frank <mrtukkin@gmail.com>
2017-02-17 20:52:37 +0100

    Cleared the repo of old files

--- newrunner.py
+++
@@ -1,7 +0,0 @@
-import ba
-
-sourcedir = 'data/datasets/pascalparts/Annotations_Part/'
-
-ppset = ba.PascalPartSet('pascpart', sourcedir, 'stern', 'aeroplane')
-# ppset.saveSegmentations()
-ppset.saveBoundingBoxes('data/datasets/voc2010/JPEGImages/', negatives=2)
